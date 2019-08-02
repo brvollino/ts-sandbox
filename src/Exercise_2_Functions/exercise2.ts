488148ade8c390b71655a0cfdbdc56c00ff1cc7c
Bruno Vollino <bruno.vollino@ilegra.com>
2019-08-02 10:14:40 -0300

    Solution to exercise 2

--- src/Exercise_2_Functions/exercise2.ts
+++ src/Exercise_2_Functions/exercise2.ts
@@ -14,15 +14,15 @@ export default () => {
   // • Add explicit parameter types and return type
   // • Fix any errors resulting from invalid types
 
-  function add(x, y) {
+  function add(x: number, y: number): number {
     return x + y;
   }
 
-  function sumArray(numbers) {
+  function sumArray(numbers: number[]): number {
     return numbers.reduce(add, 0);
   }
 
-  const someSum = sumArray(['3', '6', '9']);
+  const someSum: number = sumArray([3, 6, 9]);
 
   console.log('[Exercise 2.1]', `3 + 6 + 9 === ${someSum}`);
 
@@ -30,10 +30,14 @@ export default () => {
   // Instructions:
   // • Add explicit parameter types and return types to the `deposit` function
   // • Make the function's `message` parameter *optional*
+  interface BankAccount {
+    money: number;
+    deposit(value: number, message?: string): void;
+  };
 
-  const bankAccount = {
+  const bankAccount: BankAccount = {
     money: 0,
-    deposit(value, message) {
+    deposit(value: number, message?: string): void {
       this.money += value;
       if (message) {
         console.log(message);
@@ -51,13 +55,13 @@ export default () => {
   // Instructions:
   // • Add type annotations wherever possible
 
-  function computeScore(word) {
+  function computeScore(word: string): number {
     const letters = word.toUpperCase().split('');
-    return letters.reduce((accum, curr) => accum += getPointsFor(curr), 0);
+    return letters.reduce((accum: number, curr: string) => accum += getPointsFor(curr), 0);
   }
 
-  function getPointsFor(letter) {
-    const lettersAndPoints = [
+  function getPointsFor(letter: string): number {
+    const lettersAndPoints: [string, number][] = [
       ['AEOIULNRST', 1],
       ['DG', 2],
       ['BCMP', 3],
@@ -67,8 +71,8 @@ export default () => {
       ['QZ', 10],
     ];
 
-    return lettersAndPoints.reduce((computedScore, pointsTuple) => {
-      const [letters, score] = pointsTuple;
+    return lettersAndPoints.reduce((computedScore: number, pointsTuple: [string, number]) => {
+      const [letters, score]: [string, number] = pointsTuple;
       if (letters.split('').find((ll) => ll === letter)) {
         return computedScore += score;
       }
@@ -83,12 +87,12 @@ export default () => {
   // • Add explicit parameter types and return types
   // • Add a default greeting: "hello"
 
-  function greet(greeting) {
+  function greet(greeting: string = 'hello'): string {
     return greeting.toUpperCase();
   }
 
-  const defaultGreeting = greet();
-  const portugueseGreeting = greet('Oi como vai!');
+  const defaultGreeting: string = greet();
+  const portugueseGreeting: string = greet('Oi como vai!');
 
   console.log('[Exercise 2.4]', defaultGreeting, portugueseGreeting);
 
@@ -97,11 +101,11 @@ export default () => {
   // • Add parameter type annotation
   // • Even though this function doesn't return, add an explicit return type
 
-  function layEggs(quantity, color) {
+  function layEggs(quantity: number, color: string): void {
     console.log(`[Exercise 2.5] You just laid ${quantity} ${color} eggs. Good job!`);
   }
 
-  layEggs();
+  layEggs(7, 'green');
 
   // ======== Exercise 2.6 ========
   // Here we've initialized two variables with function types.
@@ -112,13 +116,13 @@ export default () => {
   let multiply: (val1: number, val2: number) => number;
   let capitalize: (val: string) => string;
 
-  multiply = function(value: string): string {
-    return `${value.charAt(0).toUpperCase()}${value.slice(1)}`;
-  }
-
-  capitalize = function(x: number, y: number): number {
+  multiply = function(x: number, y: number): number {
     return x * y;
-  }
+  };
+
+  capitalize = function(value: string): string {
+    return `${value.charAt(0).toUpperCase()}${value.slice(1)}`;
+  };
 
   console.log('[Exercise 2.6]', capitalize(`nifty ${multiply(5, 10)}`));
 
@@ -141,22 +145,24 @@ export default () => {
 
   const numberCollection: number[] = [];
   const stringCollection: string[] = [];
+  const booleanCollection: boolean[] = [];
+  const arraysCollection: [][] = [];
   
-  function pushToCollection(item, collection) {
+  function pushToCollection<T>(item: T, collection: T[]) {
     collection.push(item);
     return collection;
   }
   
   // Add some stuff to the collections
-  pushToCollection(false, stringCollection);
+  pushToCollection(false, booleanCollection);
   pushToCollection('hi', stringCollection);
-  pushToCollection([], stringCollection);
+  pushToCollection([], arraysCollection);
 
-  pushToCollection('1', numberCollection);
-  pushToCollection('2', numberCollection);
-  pushToCollection('3', numberCollection);
+  pushToCollection(1, numberCollection);
+  pushToCollection(2, numberCollection);
+  pushToCollection(3, numberCollection);
 
-  const incrementedByTwo = numberCollection.map((num) => num + 2);
+  const incrementedByTwo = numberCollection.map((num: number) => num + 2);
   
   console.log('[Exercise 2.7]', `[${incrementedByTwo}] should deeply equal [3,4,5]`);
 }
