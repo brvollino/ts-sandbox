05a6d46fc00848a9bcb02d61fa1a075f69193794
Bruno Vollino <bruno.vollino@ilegra.com>
2019-08-02 10:24:59 -0300

    Solution to exercise 3

--- src/Exercise_3_Classes/exercise3.ts
+++ src/Exercise_3_Classes/exercise3.ts
@@ -14,7 +14,7 @@ export default () => {
   // • Add explicit return type to the greet method
 
   class MC {
-    greet(event = 'party') {
+    greet(event: string = 'party'): string {
       return `Welcome to the ${event}`;
     }
   }
@@ -29,7 +29,10 @@ export default () => {
   // • Add typed parameters for storing values
 
   class Person {
-    constructor(name, age) {
+    public name: string;
+    private age: number;
+
+    constructor(name: string, age: number) {
       this.name = name;
       this.age = age;
     }
@@ -46,12 +49,7 @@ export default () => {
   // • Reduce class to three lines of code while maintaining functionality
 
   class Employee {
-    title: string;
-    salary: number;
-    constructor(title: string, salary: number) {
-      this.title = title;
-      this.salary = salary;
-    }
+    constructor(public title: string, public salary: number) {}
   }
 
   const employee = new Employee('Engineer', 100000);
@@ -66,41 +64,43 @@ export default () => {
   // • Make the Pony class inherit from Animal
   // • Make it so that the name member cannot be publicly accessed
 
-  class Animal {
-    constructor(name) {}
-    move(meters) {
+  abstract class Animal {
+    constructor(private name: string) {}
+    move(meters: number): void {
       console.log(`${this.name} moved ${meters}m.`);
     }
   }
 
-  class Snake {
-    move(meters) {
+  class Snake extends Animal {
+    move(meters: number = 5): void {
       console.log('Slithering...');
       // should call on parent's `move` method, w/ a default
       // slither of 5 meters
+      super.move(meters);
     }
   }
 
-  class Pony {
-    move(meters) {
+  class Pony extends Animal {
+    move(meters: number = 60) {
       console.log('Galloping...');
       // should call on parent's `move` method, w/ a default
       // gallop of 60 meters
+      super.move(meters);
     }
   }
 
   // The class Animal should not be instantiable.
   // Delete or comment out once the desired error is achieved.
-  const andrew = new Animal("Andrew the Animal");
-  andrew.move(5);
+  //const andrew = new Animal("Andrew the Animal");
+  //andrew.move(5);
 
   const sammy = new Snake("Sammy the Snake");
   sammy.move();
-  console.log(sammy.name); // Should return error
+  //console.log(sammy.name); // Should return error
 
   const pokey = new Pony("Pokey the Pony");
   pokey.move(34);
-  console.log(pokey.name); // Should return error
+  //console.log(pokey.name); // Should return error
 
   // ======== Exercise 3.5 ========
   // Goals:
@@ -108,7 +108,7 @@ export default () => {
   //   manufacturer member
 
   class Furniture {
-    constructor(manufacturer: string = 'IKEA') {}
+    constructor(protected manufacturer: string = 'IKEA') {}
   }
 
   class Desk extends Furniture {
@@ -125,18 +125,18 @@ export default () => {
 
   const desk = new Desk();
   desk.kind();
-  desk.manufacturer; // Should return error
+  //desk.manufacturer; // Should return error
 
   const chair = new Chair();
   chair.kind();
-  chair.manufacturer; // Should return error
+  //chair.manufacturer; // Should return error
 
   // ======== Exercise 3.6 ========
   // Goals:
   // • Eliminate the error without changing references to `Student.school`
 
   class Student {
-    public school: string = 'Harry Herpson High School';
+    public static school: string = 'Harry Herpson High School';
     constructor(private name: string) {};
     introduction() {
       console.log('[Exercise 3.6]', `Hi, my name is ${this.name} and I attend ${Student.school}`);
